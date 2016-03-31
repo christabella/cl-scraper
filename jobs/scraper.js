4a75bfc4b70d4b3dd3fb90ecb09d2e4991429a9f
Christabella Irwanto <christabella.irwanto@gmail.com>
2016-03-31 10:24:28 +0800

    confused jobs with for sale, oops :open_mouth:

---
+++ jobs/scraper.js
@@ -0,0 +1,34 @@
+var Promise        = require('bluebird')
+,   fs             = require('fs')
+,   Xray           = require("x-ray")
+,   xray           = new Xray().delay('2s', '10s')
+,   scrapedData    = {}
+,   categories = require('./categories.json');
+
+categories.forEach(function(category) {
+  var cat = category.category.replace(/ \/ /g,", "); 
+
+
+  var rStream = xray(category.link, 'p.row', [{
+      title: 'a.hdrlnk',
+      price: 'span.price',
+      href: 'span.txt>span.pl>a@href',
+      time: 'time@datetime',
+      location: 'span.l2 small',
+      picture_present: 'span.p',
+      link: '.pl a@href',
+        // image: xray('a@href', [{title: 'title'}])
+        // image: function($, cb2) {
+        //   return x($(this), 'title', [{
+        //     name: ''
+        //   }])(cb2);
+        // }
+    }])
+    (function(err, result){
+        if(err) {
+          console.log('Error reading from '+url+' : ', err);
+        }
+    })
+    .paginate('a.button.next@href')
+    .write("for_sale/newdata/" + cat + ".json");
+  });
